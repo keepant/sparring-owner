bbf70f04e77ba89446253a0dbcccff6d0fd73144
Irfan Dwi Prasetyo <keepant@gmail.com>
2020-06-30 21:30:45 +0700

    added list court

--- lib/pages/more/court/court.dart
+++ lib/pages/more/court/court.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sparring_owner/api/api.dart';
+import 'package:sparring_owner/components/court_card.dart';
+import 'package:sparring_owner/components/loading.dart';
+import 'package:sparring_owner/graphql/owner.dart';
 import 'package:sparring_owner/pages/more/court/add_court.dart';
 
-class Court extends StatelessWidget {
+class Court extends StatefulWidget {
   Court({
     Key key,
   }) : super(key: key);
+
+  @override
+  _CourtState createState() => _CourtState();
+}
+
+class _CourtState extends State<Court> {
+  SharedPreferences sharedPreferences;
+  String _userId;
+
+  _getUserId() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    setState(() {
+      _userId = (sharedPreferences.getString("userId") ?? '');
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getUserId();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +41,24 @@ class Court extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("Court"),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black54,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: Colors.white,
+        title: Text(
+          "Court",
+          style: TextStyle(
+            color: Colors.black54,
+            fontWeight: FontWeight.bold,
+            fontSize: 21.0,
+          ),
+        ),
         centerTitle: true,
         actions: <Widget>[
           IconButton(
@@ -25,107 +69,163 @@ class Court extends StatelessWidget {
                 withNavBar: false,
               );
             },
-            icon: Icon(Icons.add),
+            icon: Icon(
+              Icons.add,
+              color: Colors.black54,
+            ),
           )
         ],
       ),
-      body: Container(
-        height: size.height,
-        width: size.width,
-        decoration: BoxDecoration(
-          color: Color(0xfff1eefc),
-        ),
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.only(bottom: 20.0),
-                      width: 150.0,
-                      height: 150.0,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage("assets/img/field.png"),
-                          fit: BoxFit.fill,
+      body: GraphQLProvider(
+        client: API.client,
+        child: Query(
+          options: QueryOptions(
+              documentNode: gql(getOwnerCourt),
+              pollInterval: 5,
+              variables: {
+                'id': _userId,
+              }),
+          builder: (QueryResult result,
+              {FetchMore fetchMore, VoidCallback refetch}) {
+            if (result.loading) {
+              return Loading();
+            }
+
+            if (result.hasException) {
+              return Center(
+                child: Text(result.exception.toString()),
+              );
+            }
+
+            var courtList = result.data['owners'][0]['courts'];
+
+            return _userId == null
+                ? Loading()
+                : courtList.length < 1
+                    ? emptyCourt()
+                    : Container(
+                        height: size.height,
+                        width: size.width,
+                        decoration: BoxDecoration(
+                          color: Color(0xfff1eefc),
                         ),
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.only(bottom: 15.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            "You don\'t have any court yet!",
-                            style: TextStyle(
-                              fontSize: 22.0,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            textAlign: TextAlign.center,
-                            softWrap: true,
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Icon(
-                            FontAwesomeIcons.solidTimesCircle,
-                            color: Colors.red,
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 50.0),
-                      child: Text(
-                        "Add your court to manage",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 16.0, height: 1.6),
-                      ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 10, bottom: 50.0),
-                      child: Column(
-                        children: <Widget>[
-                          GestureDetector(
-                            child: Container(
-                              padding: EdgeInsets.symmetric(vertical: 15.0),
-                              margin: EdgeInsets.only(bottom: 30.0),
-                              width: size.width - 50.0,
-                              decoration: BoxDecoration(
-                                color: Theme.of(context).primaryColor,
-                                borderRadius: BorderRadius.circular(180.0),
-                              ),
-                              child: Text(
-                                "Add court now!",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xffffffff),
-                                ),
-                              ),
-                            ),
-                            onTap: () {
-                              pushNewScreen(
-                                context,
-                                screen: AddCourt(),
-                                withNavBar: false,
+                        child: SafeArea(
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: courtList.length,
+                            itemBuilder: (context, index) {
+                              var court = courtList[index];
+                              var img = courtList[index]['court_images'][0];
+                              return CourtCard(
+                                onTap: () {},
+                                imgUrl: img['name'],
+                                title: court['name'],
+                                location: court['address'],
                               );
                             },
                           ),
-                        ],
-                      ),
-                    )
-                  ],
+                        ),
+                      );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget emptyCourt() {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height,
+      width: size.width,
+      decoration: BoxDecoration(
+        color: Color(0xfff1eefc),
+      ),
+      child: SafeArea(
+        child: Center(
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  width: 150.0,
+                  height: 150.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/img/field.png"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
                 ),
-              ),
-            ],
+                Container(
+                  margin: EdgeInsets.only(bottom: 15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "You don\'t have any court yet!",
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                        softWrap: true,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Icon(
+                        FontAwesomeIcons.solidTimesCircle,
+                        color: Colors.red,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 50.0),
+                  child: Text(
+                    "Add your court to manage",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16.0, height: 1.6),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 10, bottom: 50.0),
+                  child: Column(
+                    children: <Widget>[
+                      GestureDetector(
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 15.0),
+                          margin: EdgeInsets.only(bottom: 30.0),
+                          width: size.width - 50.0,
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).primaryColor,
+                            borderRadius: BorderRadius.circular(180.0),
+                          ),
+                          child: Text(
+                            "Add court now!",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xffffffff),
+                            ),
+                          ),
+                        ),
+                        onTap: () {
+                          pushNewScreen(
+                            context,
+                            screen: AddCourt(),
+                            withNavBar: false,
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
